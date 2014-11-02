4a26cd70455ca110b668023a8ad303753ec19835
Gonçalo Cardoso <goncalo.cardoso@stepvalue.com>
2014-11-02 13:56:22 +0000

    Connection to MongoDB

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
  * Module dependencies.
  */
 var express = require('express')
-  , io = require('socket.io')
-  , http = require('http')
-  , twitter = require('ntwitter')
-  , cronJob = require('cron').CronJob
-  , _ = require('underscore')
-  , path = require('path');
+	, io = require('socket.io')
+	, http = require('http')
+	, twitter = require('ntwitter')
+	, cronJob = require('cron').CronJob
+	, _ = require('underscore')
+	, path = require('path')
+	, mongo = require('mongodb').MongoClient
+	, assert = require('assert');
+
+var mongoUrl = 'mongodb://localhost:27017/gcardoso';
+
+var portfolioList = [];
 
 //Create an express app
 var app = express();
@@ -20,12 +26,14 @@ var watchSymbols = ['#gcardoso'];
 
 //This structure will keep the total number of tweets received and a map of all the symbols and how many tweets received of that symbol
 var watchList = {
-  total: 0,
-  symbols: {}
+	total: 0,
+	symbols: {}
 };
 
 //Set the watch symbols to zero.
-_.each(watchSymbols, function(v) { watchList.symbols[v] = 0; });
+_.each(watchSymbols, function (v) {
+	watchList.symbols[v] = 0;
+});
 
 //Generic Express setup
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
@@ -42,18 +50,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 //We're using bower components so add it to the path to make things easier
 app.use('/components', express.static(path.join(__dirname, 'components')));
 
+var mongoUrl = 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/gcardoso';
+
+var enviromnent = app.get('env');
+
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+if ('development' == enviromnent) {
+	app.use(express.errorHandler());
+	mongoUrl = 'mongodb://localhost:27017/gcardoso';
 }
 
 
 //Our only route! Render it with the current watchList
-app.get('/', express.basicAuth('gcardoso89', 'timesUP32'),  function(req, res) {
-  res.render('index.html');
+app.get('/', express.basicAuth('gcardoso89', 'timesUP32'), function (req, res) {
+
+	mongo.connect(mongoUrl, function (err, db) {
+
+		if(('development' != enviromnent)) db.auth('admin', 'VPSH3mpQp6fH');
+
+		var collection = db.collection('portfolio');
+
+		collection.find({}).toArray(function (err, docs) {
+			//assert.equal(err, null);
+			//assert.equal(4, docs.length);
+			portfolioList = docs;
+
+			for (var i = 0; i < portfolioList.length; i++) {
+				var obj = portfolioList[i];
+				for (var prop in obj) {
+					var col = obj[prop];
+					if (col == "null" || col == 'null' || col == null) portfolioList[i][prop] = null;
+				}
+			}
+
+			res.render('index.html', {portfolio: portfolioList});
+			db.close();
+		});
+
+	});
+
 });
 
-app.get('/teste', function(req, res){
+app.get('/teste', function (req, res) {
 	res.render('teste.html');
 });
 
@@ -62,9 +100,9 @@ var sockets = io.listen(server);
 
 //Set the sockets.io configuration.
 //THIS IS NECESSARY ONLY FOR HEROKU!
-sockets.configure(function() {
-  sockets.set('transports', ['xhr-polling']);
-  sockets.set('polling duration', 3600);
+sockets.configure(function () {
+	sockets.set('transports', ['xhr-polling']);
+	sockets.set('polling duration', 3600);
 });
 
 //Instantiate the twitter component
@@ -72,48 +110,48 @@ sockets.configure(function() {
 //since it will instantiate a connection on my behalf and will drop all other streaming connections.
 //Check out: https://dev.twitter.com/
 var t = new twitter({
-  consumer_key: 'XHHh0St57xEb0uZ6zlVxAzgFv',           // <--- FILL ME IN
-  consumer_secret: 'qxbmnjQau0W6ofQsJeByRuIi2iGMFLW2aJMNd5aXjnTZ4Ic8tU',        // <--- FILL ME IN
-  access_token_key: '93891411-DtXySlEpuTNnM09dUEjb0aHnoj6mBrXb0gPQAgz87',       // <--- FILL ME IN
-  access_token_secret: 'mEegi29Ivz0eZJHDxwxURk32wMqbWf0CxgUJvBqWimf2g'     // <--- FILL ME IN
+	consumer_key: 'XHHh0St57xEb0uZ6zlVxAzgFv',           // <--- FILL ME IN
+	consumer_secret: 'qxbmnjQau0W6ofQsJeByRuIi2iGMFLW2aJMNd5aXjnTZ4Ic8tU',        // <--- FILL ME IN
+	access_token_key: '93891411-DtXySlEpuTNnM09dUEjb0aHnoj6mBrXb0gPQAgz87',       // <--- FILL ME IN
+	access_token_secret: 'mEegi29Ivz0eZJHDxwxURk32wMqbWf0CxgUJvBqWimf2g'     // <--- FILL ME IN
 });
 
 var arr = new Array();
-var screens = [null,null,null];
+var screens = [null, null, null];
 var nrScreems = screens.length;
 
 //Tell the twitter API to filter on the watchSymbols
-t.stream('statuses/filter', { track: watchSymbols }, function(stream) {
+t.stream('statuses/filter', { track: watchSymbols }, function (stream) {
 
-  //We have a connection. Now watch the 'data' event for incomming tweets.
-  stream.on('data', function(tweet) {
+	//We have a connection. Now watch the 'data' event for incomming tweets.
+	stream.on('data', function (tweet) {
 
-    //Make sure it was a valid tweet
-    if (tweet.text !== undefined) {
+		//Make sure it was a valid tweet
+		if (tweet.text !== undefined) {
 
-      arr.push(tweet);
+			arr.push(tweet);
 
-      var c = 0;
+			var c = 0;
 
-      for (var i = 0, n = arr.length; i < n; i++){
+			for (var i = 0, n = arr.length; i < n; i++) {
 
-        if ( c >= screens.length ) c = 0;
+				if (c >= screens.length) c = 0;
 
-        screens[c] = arr[i];
-        c++
+				screens[c] = arr[i];
+				c++
 
-      }
+			}
 
-      sockets.sockets.emit('data', screens);
-    }
-  });
+			sockets.sockets.emit('data', screens);
+		}
+	});
 });
 
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 //Create the server
-server.listen(app.get('port'), server_ip_address, function(){
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), server_ip_address, function () {
+	console.log('Express server listening on port ' + app.get('port'));
 });
 
 

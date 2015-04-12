02402b5d08eae519c166600b56c99aeac96e0281
Gonçalo Cardoso <goncalo.cb.ferreira@gmail.com>
2015-04-12 17:35:51 +0100

    Debug redirect problem

--- server.js
+++ server.js
@@ -226,7 +226,7 @@ if (production){
 	app.all(/.*/, function(req, res, next) {
 		var host = req.header("host");
 		var url = req.url;
-		console.log(host + url);
+		console.log(req);
 		if (host.match(/^www\..*/i)) {
 			next();
 		} else {
